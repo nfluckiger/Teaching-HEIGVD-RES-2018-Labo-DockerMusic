5280c8ea207e41600a9f185aa435258b94abcf51
Nathan Fluckiger <nathan.fluckiger@heig-vd.ch>
2018-05-16 08:23:58 +0200

    Une version fonction fonctionelle

---
+++ docker/protocol.js
@@ -0,0 +1,17 @@
+/*
+ * Our application protocol specifies the following default multicast address and port
+ */
+
+exports.PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
+exports.TCP_ADDRESS = "localhost";
+exports.PROTOCOL_PORT = 2205;
+
+exports.PIANO = "ti-ta-ti";
+exports.TRUMPER = "pouet";
+exports.FLUTE = "trulu";
+exports.VIOLIN = "gzi-gzi";
+exports.DRUM = "boum-boum";
+
+exports.SOUND_INTERVAL = 1000;
+
+exports.DELAY_TCP = 5000;
